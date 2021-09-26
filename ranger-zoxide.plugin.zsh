e80f6599ebc350821c6c2fea637724d91c41b87c
MuXiu1997 <muxiu1997@gmail.com>
2021-09-26 13:42:20 +0800

    Add ranger_cd

--- ranger-zoxide.plugin.zsh
+++ ranger-zoxide.plugin.zsh
@@ -1,14 +1,25 @@
 # Add a `r` function to zsh that opens ranger either at the given directory or
-# at the one autojump suggests
+# at the one zoxide suggests
+
+# https://github.com/ranger/ranger/wiki/Integration-with-other-programs#changing-directories
+ranger_cd() {
+  temp_file="$(mktemp -t "ranger_cd.XXXXXXXXXX")"
+  ranger --choosedir="$temp_file" -- "${@:-$PWD}"
+  if chosen_dir="$(cat -- "$temp_file")" && [ -n "$chosen_dir" ] && [ "$chosen_dir" != "$PWD" ]; then
+    cd -- "$chosen_dir"
+  fi
+    rm -f -- "$temp_file"
+}
+
 r() {
   if [ "$1" != "" ]; then
     if [ -d "$1" ]; then
-      ranger "$1"
+      ranger_cd "$1"
     else
-      ranger "$(zoxide query $1)"
+      ranger_cd "$(zoxide query $1)"
     fi
   else
-    ranger
+    ranger_cd
   fi
-    return $?
+	return $?
 }
